0a650996db04cb47f1bfb020d27c23e240f63961
tjasha <tjasa.spes@gmail.com>
2026-01-04 16:09:57 +0100

    add models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 //do store data from/to database
 
 // make reservation form
@@ -8,4 +10,66 @@ type Reservation struct {
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// by convenntion we change names in database first_name to FirstName
+// describes User table -> User model
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the room model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the restrictions model
+type Restrictions struct {
+	ID               int
+	RestrictionsName string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+// Reservations is reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	//we can add additional information
+	//here we add information about the Room, as it's connected
+	Room Rooms
+}
+
+// RoomRestriction is roomrestriction model
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	// additional information added
+	Room        Rooms
+	Reservation Reservations
+	Restriction Restrictions
 }
