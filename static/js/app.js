3523be7c40a41a3ef861c2a98a6f662d1ca0648d
tjasha <tjasa.spes@gmail.com>
2026-01-09 17:46:44 +0100

    connecting major's suite and extracting JS modules

---
+++ static/js/app.js
@@ -0,0 +1,117 @@
+function Prompt() {
+    let toast = function (c) {
+        const {
+            msg = "",
+            icon = "success",
+            position = "top-end",
+        } = c
+
+        const Toast = Swal.mixin({
+            toast: true,
+            title: msg,
+            position: position,
+            icon: icon,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({});
+
+    }
+
+
+    let success = function (c) {
+        const {
+            msg = "",
+            title = "",
+            footer = "",
+        } = c;
+        Swal.fire({
+            icon: "success",
+            title: title,
+            text: msg,
+            footer: footer,
+        });
+    }
+
+    let error = function (c) {
+        const {
+            msg = "",
+            title = "",
+            footer = "",
+        } = c;
+        Swal.fire({
+            icon: "error",
+            title: title,
+            text: msg,
+            footer: footer,
+        });
+    }
+
+
+    async function custom(c) {
+        const {
+            icon = "",
+            msg = "",
+            title = "",
+            showConfirmButton = true
+        } = c;
+
+        const {value: result} = await Swal.fire({
+            icon: icon,
+            title: title,
+            html: msg,
+            backdrop: false,
+            focusConfirm: false,
+            showCancelButton: true,
+            showConfirmButton: showConfirmButton,
+            willOpen: () => {
+                //logic of this function is moved to generals.page.tmpl
+                if (c.willOpen !== undefined) {
+                    c.willOpen()
+                }
+
+            },
+            didOpen: () => {
+                //logic of this function is moved to generals.page.tmpl
+                if (c.didOpen !== undefined) {
+                    c.didOpen()
+                }
+            },
+
+            preConfirm: () => {
+                return [
+                    document.getElementById('start').value,
+                    document.getElementById('end').value,
+                ]
+            }
+
+        })
+
+        if (result) {
+            if (result.dismiss !== Swal.DismissReason.cancel) {
+                if (result.value !== "") {
+                    if (c.callback !== undefined) {
+                        c.callback(result);
+                    }
+                } else {
+                    c.callback(false);
+                }
+            } else {
+                c.callback(false)
+            }
+        }
+
+    }
+
+    return {
+        toast: toast,
+        success: success,
+        error: error,
+        custom: custom,
+    }
+}
---
+++ static/js/checkAvailability.js
@@ -0,0 +1,77 @@
+function checkAvailability(roomId, csrfToken) {
+    let html = `
+    <form id="check-availability-form" action="" method="post" novalidate class="needs-validation">
+        <div class="form-row">
+            <div class="col">
+                <div class="form-row" id="reservation-dates-modal">
+                    <div class="col">
+                        <input disabled required class="form-control" type="text" name="start" id="start" placeholder="Arrival">
+                    </div>
+                    <div class="col">
+                        <input disabled required class="form-control" type="text" name="end" id="end" placeholder="Departure">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </form>
+    `
+
+    attention.custom({
+        title: 'Choose your dates',
+        msg: html,
+
+        //willOpen and didOpen are called in case.layout.tmpl
+        willOpen: () => {
+            const elem = document.getElementById('reservation-dates-modal');
+            const rp = new DateRangePicker(elem, {
+                format: 'dd-mm-yyyy',
+                showOnFocus: true,
+                minDate: new Date(),
+            })
+        },
+        didOpen: () => {
+            document.getElementById('start').removeAttribute('disabled');
+            document.getElementById('end').removeAttribute('disabled')
+        },
+
+        callback: function (result) {
+            console.log("called");
+
+            //this variable is storing form
+            let form = document.getElementById("check-availability-form");
+            //this variable is storing all elements from the form
+            let formData = new FormData(form);
+            //this is appending CSRFToken from any template that we created
+            formData.append("csrf_token", csrfToken)
+            // we add room id, which it 1
+            formData.append("room_id", roomId)
+
+            fetch('/search-availability-json', {
+                method: "post",
+                body: formData,
+            }).then(response => response.json())
+                .then(data => {
+                    if (data.ok) {
+                        attention.custom({
+                            icon: 'success',
+                            showConfirmButton: false,
+                            msg: '<p>Room is available!</p>'
+                                + '<p><a href="book-room?id='
+                                + data.room_id
+                                + '&s='
+                                + data.start_date
+                                + '&e='
+                                + data.end_date
+                                + '" class="btn btn-primary">'
+                                + 'Book now!</a></p>'
+                        })
+                    } else {
+                        attention.error({
+                            msg: "No availability"
+                        })
+                    }
+                })
+        }
+    });
+}
