d4537d14b1c73ed79aac5866242df733607231d5
tjasha <tjasa.spes@gmail.com>
2026-01-09 18:07:11 +0100

    adding migration for seeding rooms and restrictions

---
+++ migrations/20260109170428_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at)
+VALUES ('reservation', '2026-01-03 00:00:00.000', '2026-01-03 00:00:00.000'),
+       ('Owner block', '2026-01-01 00:00:00.000', '2025-01-02 00:00:00.000');
