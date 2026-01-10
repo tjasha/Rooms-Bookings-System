7bd949cb4824fa67cf4e57f9762a138917b76382
tjasha <tjasa.spes@gmail.com>
2026-01-10 22:40:26 +0100

    create authentication handler for login screen

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -39,6 +39,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
+	mux.Get("/user/login", handlers.Repo.ShowLogin)
+
 	//where all our images are saved
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
