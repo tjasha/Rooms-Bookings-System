92c117393176408f8de4985cef4744a3650c3b4f
tjasha <tjasa.spes@gmail.com>
2026-01-11 14:32:53 +0100

    database functions for editing a reservation

--- internal/repository/reporitory.go
+++ internal/repository/reporitory.go
@@ -10,24 +10,19 @@ type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
-
 	InsertRoomRestriction(res models.RoomRestriction) error
-
 	SearchAvailabilityForDatesByRoomID(start, end time.Time, roomID int) (bool, error)
-
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
-
 	GetRoomById(id int) (models.Room, error)
 
 	GetUserByID(id int) (models.User, error)
-
 	UpdateUser(u models.User) error
-
 	Authenticate(email, testPassword string) (int, string, error)
 
 	AllReservations() ([]models.Reservation, error)
-
 	AllNewReservations() ([]models.Reservation, error)
-
 	GetReservationById(id int) (models.Reservation, error)
+	UpdateReservation(r models.Reservation) error
+	DeleteReservation(id int) error
+	UpdateProcessedForReservation(id, processed int) error
 }
