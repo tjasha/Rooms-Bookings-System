e2068b5211c2533fc8254e2261701597ff362647
tjasha <tjasa.spes@gmail.com>
2026-01-11 10:34:22 +0100

    creating admin template and stub handlers for admin functionality

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -53,6 +53,10 @@ func routes(app *config.AppConfig) http.Handler {
 		// all routes added here will use Auth middleware
 		mux.Use(Auth)
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+
+		mux.Get("/reservations-new", handlers.Repo.AdminNewReservations)
+		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 	})
 
 	return mux
