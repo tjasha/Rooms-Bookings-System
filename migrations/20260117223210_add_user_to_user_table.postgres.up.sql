0655c885f6c34627869899a3505ce365752b33c9
tjasha <tjasa.spes@gmail.com>
2026-01-17 23:47:36 +0100

    adding admin user to migration

---
+++ migrations/20260117223210_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.users
+(first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES ('Admin', 'user', 'admin@admin.com', '$2a$12$wiP/0RqtUErm4u5ZuWDUtulBJMEa74CitvCNdqlS87A1Q03XJ9ysu', 3,
+        '2026-01-01 00:00:00.000', '2026-01-01 00:00:00.000');
